e816fdc453a27772229794f0eb5c1bcac8226f3b
SeulYoung <38755691+SeulYoung@users.noreply.github.com>
2023-06-25 22:04:42 +0800

    Create reversePrint.h

---
+++ list/easy/reversePrint.h
@@ -0,0 +1,46 @@
+//
+// Created by 94817 on 2023/6/24.
+//
+
+#pragma once
+
+#include <vector>
+
+struct ListNode {
+    int val;
+    ListNode *next;
+
+    ListNode(int x) : val(x), next(nullptr) {}
+};
+
+class Solution {
+public:
+    std::vector<int> reversePrint(ListNode *head) {
+        result_.clear();
+        dfs(head);
+        return result_;
+    }
+
+private:
+    void dfs(ListNode *node) {
+        if (!node) {
+            return;
+        }
+        dfs(node->next);
+        result_.push_back(node->val);
+    }
+
+    std::vector<int> result_;
+};
+
+class Solution1 {
+public:
+    std::vector<int> reversePrint(ListNode *head) {
+        std::vector<int> result;
+        while (head) {
+            result.insert(result.begin(), head->val);
+            head = head->next;
+        }
+        return result;
+    }
+};
