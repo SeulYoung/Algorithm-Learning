c79a87457cde9de6561b93a771a7478bb175a633
SeulYoung <38755691+SeulYoung@users.noreply.github.com>
2023-07-09 14:58:55 +0800

    Create deleteNode.h

---
+++ dual-pointer/easy/deleteNode.h
@@ -0,0 +1,38 @@
+//
+// 剑指 Offer 18. 删除链表的节点
+//
+
+#pragma once
+
+struct ListNode {
+    int val;
+    ListNode *next;
+
+    ListNode(int x) : val(x), next(nullptr) {}
+};
+
+class Solution {
+public:
+    ListNode *deleteNode(ListNode *head, int val) {
+        if (!head) {
+            return head;
+        }
+
+        if (head->val == val) {
+            head = head->next;
+            return head;
+        }
+
+        ListNode *pre = head;
+        ListNode *cur = head->next;
+        while (cur) {
+            if (cur->val == val) {
+                pre->next = cur->next;
+                return head;
+            }
+            pre = cur;
+            cur = cur->next;
+        }
+        return head;
+    }
+};
